64511bb896978b23f4f1d6344ce255064c87774d
mafsimonsani <104512807+mafsimonsani@users.noreply.github.com>
2023-12-10 22:48:50 +0100

    Code

---
+++ core_drill.py
@@ -0,0 +1,10 @@
+# DMOPC '14 Contest 5 P1 Core Drill
+PI = 3.141592653589793
+
+radius = int(input())
+height = int(input())
+
+# Calcul of the volume of the right circular cone.
+volume = (PI * radius ** 2 * height) / 3
+
+print(volume)
